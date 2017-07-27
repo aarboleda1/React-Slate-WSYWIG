9fdbcb62ac79eb4a1b1d0411af2bc9d7d2871ff9
Anton <anton.arboleda@gmail.com>
2017-07-26 17:18:30 -0700

    folder cleanup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import Store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const StoreInstance = Store();
+
+ReactDOM.render( <Provider store={StoreInstance}>
+   <App />
+ </Provider>, document.getElementById('root'));
 registerServiceWorker();
--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-import rootReducer from  './reducers';
+import rootReducer from  './reducers/reducers.js';
 export default(initialState) => {
     return createStore(rootReducer, initialState);
 }
