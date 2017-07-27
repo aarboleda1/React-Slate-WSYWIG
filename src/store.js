73eb976ffa3b819df2eb7bd6b0407724e4527c6a
Anton <anton.arboleda@gmail.com>
2017-07-27 10:49:18 -0700

    redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,5 +1,10 @@
 import { createStore } from 'redux';
 import rootReducer from  './reducers/reducers.js';
+const initialState = {test: '123'}
 export default(initialState) => {
-    return createStore(rootReducer, initialState);
+    return createStore(
+			rootReducer, 
+			initialState,
+			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		);
 }
